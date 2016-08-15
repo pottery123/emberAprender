6651c0767a72045e6f326e4b90039404dd7d80b3
Sarah <sarahkane682@yahoo.com>
2016-08-15 15:27:48 -0700

    am using my rails api hosted by heroku yes

--- app/routes/location.js
+++ app/routes/location.js
@@ -5,8 +5,9 @@ export default Ember.Route.extend({
 model(params) {
   let number = params.location_id
   //let pageNumber = title || 1
-  return this.get('ajax').request('http://localhost:3000/locations/' + number);
+  return this.get('ajax').request('https://aprenderapi.herokuapp.com/locations/' + number);
 },
+
 
 queryParams:{
   number:{
