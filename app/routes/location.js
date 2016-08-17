1e7ae577f7b4f7a4bc661289fec450182c346ecd
Sarah <sarahkane682@yahoo.com>
2016-08-17 16:48:02 -0700

    lets hope nesting the routes work

--- app/routes/location.js
+++ app/routes/location.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
   ajax: Ember.inject.service(),
 model(params) {
   let number = params.location_id
-  let language = this.get('applicationController').get("languageName")
+  //let language = this.get('applicationController').get("languageName")
   //let pageNumber = title || 1
   return this.get('ajax').request('http://localhost:3000/locations/' + number);
 },
